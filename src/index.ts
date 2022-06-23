5c59f13bd7c533437d91911ee8fb40933e1ecd99
eduwr <eduwric@gmail.com>
2022-06-22 22:16:18 -0300

    project setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const PORT = 3001;
+
+const app = express()
+const router = express.Router()
+
+app.use(express.json())
+
+router.get("/", (req, res) => {
+  res.send({hello: "Worlds"})
+})
+
+app.use(router)
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
