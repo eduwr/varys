06ce7db822b13501ddc92a16daa619f8763e4655
eduwr <eduwric@gmail.com>
2022-06-22 23:04:26 -0300

    feat: remove mock and add post route

--- src/routes.ts
+++ src/routes.ts
@@ -2,42 +2,33 @@ import { Router, Request } from 'express'
 
 const router = Router()
 
-const input = `
-import "VarysContractBase.sol";
-import "VarysContractExtras.sol";
-
-contract VarysContract {
-  mapping (uint => address) public addresses;
-}
-`
-
 const getLines = (input: string) => input.split(/\n/).filter(Boolean);
 const getImports = (lines: string[]) => lines.filter(line => line.includes("import"));
 const getContract = (lines: string[]) => lines.filter(line => line.includes("contract"));
 const importCleanRegEx = /import|\s|;|"/ig
-const contractCleanRegEx = /\s|contract|{|}/ig
 
 
-router.get("/", (req, res) => {
-  res.send({ hello: "Worlds" })
-})
+router.post("/analyze", (req: Request, res) => {
 
-type RequestBody = {
-  code: string
-}
+  console.log(req)
 
-router.get("/analyze", (req: Request, res) => {
-  const lines = getLines(input);
+  const { code } = req.body
+
+  if (!code) {
+    res.status(400)
+    res.send({ error: "Bad Request" })
+  }
+
+  const lines = getLines(code);
   const imports = getImports(lines).map((raw) => raw.replace(importCleanRegEx, ""));
-  const contracts = getContract(lines).map(raw => raw.replace(contractCleanRegEx, ""));
- 
+  const contracts = getContract(lines).map(raw => raw.trim().split(" ")[1]);
+
   res.status(200)
 
   res.send({
     imports,
-    contracts
+    contracts,
   })
- 
 });
 
 export default router
